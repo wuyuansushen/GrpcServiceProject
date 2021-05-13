8fd6acd4ebcd4ba71e547c8ff0cbc10b551d5334
wuyuansushen <wuyuansushen@gmail.com>
2021-05-13 13:44:52 +0800

    Run in local machine with MariaDB MS 32-bit 10.3 version

--- gRPCClient/Program.cs
+++ gRPCClient/Program.cs
@@ -10,9 +10,9 @@ namespace GrpcClient
         static async Task Main(string[] args)
         {
             // The port number(5001) must match the port of the gRPC server.
-            using var channel = GrpcChannel.ForAddress("https://grpc.fiveelementgod.xyz:443");
+            //using var channel = GrpcChannel.ForAddress("https://grpc.fiveelementgod.xyz:443");
             //using var channel = GrpcChannel.ForAddress("https://127.0.0.1:5001");
-            //using var channel = GrpcChannel.ForAddress("http://127.0.0.1:6000");
+            using var channel = GrpcChannel.ForAddress("http://127.0.0.1:6000");
             var client =  new Greeter.GreeterClient(channel);
             var reply = await client.SayHelloAsync(
                               new HelloRequest { Name = "Gayhub" });
